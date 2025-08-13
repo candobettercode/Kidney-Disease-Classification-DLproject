d836dfd412715c882481cf3c7aadb44a82f45567
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2025-08-13 16:26:40 +0530

    base model created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from src import logging
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline  
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -7,6 +8,19 @@ try:
     logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+    logging.info(f"*******************")
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logging.exception(e)
