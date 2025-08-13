cec117c0437f7b4725459f1ee383882a1d536f4b
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2025-08-13 11:45:43 +0530

    Data ingestion completed

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from src import logging
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline  
 
-logging.info("Welcome to our custome log")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
+    raise e
